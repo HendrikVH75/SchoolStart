1f35312b7c80920f108b5c6c762295fe9cbaa89a
HendrikVH75 <hendrikvh@web.de>
2023-08-08 14:05:32 +0200

    Start

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        double baguetteLaenge;
+        double baguetteRadius;
+        double baguetteMenge;
+        double baguetteGrundflaeche;
+        double baguetteVolumen;
+        double backZuwachs;
+        double teigMenge;
+
+        System.out.println("Länge des Baguette: ");
+        baguetteLaenge = scan.nextDouble();
+
+        System.out.println("Radius des Baguette: ");
+        baguetteRadius = scan.nextDouble();
+
+        System.out.println("Menge der Baguettes: ");
+        baguetteMenge = scan.nextDouble();
+
+        // Grundfläche = Pi * Radius(Quadrat)
+        baguetteGrundflaeche = Math.PI * Math.pow( baguetteRadius, 2);
+        System.out.println("Grundfläche des Baguette: " + Math.ceil(baguetteGrundflaeche));
+
+        baguetteVolumen = baguetteGrundflaeche * baguetteLaenge;
+        System.out.println("Volumen des Baguette: " + Math.ceil(baguetteVolumen));
+
+        teigMenge = baguetteVolumen * baguetteMenge;
+        System.out.println("Teigmenge: " + Math.ceil(teigMenge));
+
+        backZuwachs = ((teigMenge * 35) / 100);
+        System.out.println("Backzuwachs des Baguette: " + Math.ceil(backZuwachs));
+
+        teigMenge = teigMenge - backZuwachs;
+        System.out.println("Tatsächliche Teigmenge: " + Math.ceil(teigMenge));
+
+    }
+
+}
